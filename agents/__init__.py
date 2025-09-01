aa7bab7a3f0579d629fc7af85adefa24c18bd7df
Aditya KUMAR2 <aditya.kumar2@amadeus.com>
2025-09-01 12:35:09 +0530

    Creating Flight Offers Agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -7,5 +7,6 @@ using LangGraph to provide comprehensive travel support.
 
 from .base_agent import BaseAgent
 from .visa_agent import VisaAgent
+from .flight_offers_agent import FlightOffersAgent
 
-__all__ = ["BaseAgent", "VisaAgent"]
+__all__ = ["BaseAgent", "VisaAgent", "FlightOffersAgent"]
